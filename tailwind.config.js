fb91eb86ab15d8cc08036dcd56c45333215c1afa
pat200213 <caniliem76@gmail.com>
2024-05-01 13:49:27 +0800

    default landing page

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px'
+    },
+    extend: {
+      colors: {
+        primary: '#192346',
+        secondary: '#FE5301'
+
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      }   
+    },
+  },
+  plugins: [],
+}
+
